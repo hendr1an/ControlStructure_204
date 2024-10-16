91b7f6cbf5c5fccf142057c7021564bbcae1313e
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-10-16 21:27:27 +0700

    1

---
+++ PengurutanNilai.py
@@ -0,0 +1,12 @@
+percentage = float(input("input score: "))
+
+if percentage >= 90:
+    print(" Excellent performance")
+elif percentage >= 80:
+    print ("very good performance")
+elif percentage >= 70:
+    print("Good performance")
+elif percentage >= 60:
+    print("Average performance")
+else:
+    print("Needs Improvement")
